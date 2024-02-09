731bd42d76bfee4c6b85406ab300f5992fa1538b
Connie Ye <constanceye1316@gmail.com>
2024-02-10 07:18:05 +0800

    Update processInfo.js

--- scripts/processInfo.js
+++ scripts/processInfo.js
@@ -11,11 +11,18 @@ fs.createReadStream("scripts/rawInfo.csv")
     const d3 = data["dialogue 3"];
     const d4 = data["dialogue 4"];
     const d5 = data["dialogue 5"];
+
+    const d1_override = data["round 1 override"];
+    const d2_override = data["round 2 override"];
+    const d3_override = data["round 3 override"];
+    const d4_override = data["round 4 override"];
+    const d5_override = data["round 5 override"];
+
     const final = data["final dialogue"];
     const angry = data["angry message"];
     const ending = data["ending music"] + ".mp3";
 
-    rats.push({
+    const rat = {
       name,
       filename: ratid,
       tagline,
@@ -25,11 +32,37 @@ fs.createReadStream("scripts/rawInfo.csv")
       zodiac,
       dialogue: [d1, d2, d3, d4, d5, final],
       angry,
-    });
+    };
+
+    if (
+      d1_override ||
+      d2_override ||
+      d3_override ||
+      d4_override ||
+      d5_override
+    ) {
+      const fallback = `${ratid}.png`;
+      rat.talking_to_rats_filename = [
+        d1_override || fallback,
+        d2_override || fallback,
+        d3_override || fallback,
+        d4_override || fallback,
+        d5_override || fallback,
+      ];
+    }
+
+    rats.push(rat);
   })
   .on("end", () => {
-    console.log(rats);
-    fs.writeFile("src/data/rats.json", JSON.stringify(rats, null, 4), (err) => {
-      console.log(err);
+    const sortedRats = rats.sort((rata, ratb) => {
+      rata.ratid > ratb.ratid;
     });
+    console.log(sortedRats);
+    fs.writeFile(
+      "src/data/rats.json",
+      JSON.stringify(sortedRats, null, 4),
+      (err) => {
+        console.log(err);
+      }
+    );
   });
